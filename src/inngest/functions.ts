8088fdc1a030a76dfa8fd267cb6843ad2a45aec3
Aman Raidas <128576848+igamanraj@users.noreply.github.com>
2025-09-13 20:32:04 +0530

    Integrated docker, E2B - Sandboxes background Jobs

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,23 +1,36 @@
 import {  gemini, createAgent } from "@inngest/agent-kit";
-
 import { inngest } from "./client";
+import { Sandbox , } from "@e2b/code-interpreter"
+import { getSandbox } from "./utils";
+
+
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event}) => {
-    
+  async ({ event, step }) => {
+
+    const sandboxId = await step.run("get-sandbox-id", async() => {
+      const sandbox = await Sandbox.create("vibe-nextjs-tests-2");
+      return sandbox.sandboxId;
+
+    });
      // Create a new agent with a system prompt (you can add optional tools, too)
     const summarizer = createAgent({
-      name: "summarizer",
+      name: "code-agent",
       system: "You are an expert next.js developer. you write readable, maintainable code. You are write simple next.js & react snippets.",
       model: gemini({ model: "gemini-1.5-flash", }),
     });
 
     const { output } = await summarizer.run(
-  `Write the following snippet: ${event.data.value}`
-);
+  `Write the following snippet: ${event.data.value}`);
 
 
-    return { output };
+  const sandboxUrl = await step.run("get-sandbox-url", async() => {
+    const sandbox = await getSandbox(sandboxId);
+    const host = sandbox.getHost(3000);
+    return `http://${host}`;
+  });
+
+    return { output, sandboxUrl };
   },
 );
