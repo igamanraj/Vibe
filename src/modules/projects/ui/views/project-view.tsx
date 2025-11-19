c826be3f1f38cdb027a6f980986153826e6c816b
igamanraj <amanra812727@gmail.com>
2025-11-19 18:04:50 +0530

    feat: integrate theme provider and enhance project view with loading states and fragment management

--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -1,7 +1,6 @@
 "use client";
-
-import { useTRPC } from "@/trpc/client";
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { Suspense, useState } from "react";
+import { Fragment } from "@/generated/prisma";
 
 import {
   ResizableHandle,
@@ -9,13 +8,14 @@ import {
   ResizablePanelGroup,
 } from "@/components/ui/resizable";
 import { MessagesContainer } from "../components/messages-container";
-import { Suspense } from "react";
+import { ProjectHeader } from "../components/project-header";
 
 interface Props {
   projectId: string;
 }
 
 export const ProjectView = ({ projectId }: Props) => {
+  const [activeFragment, setActiveFragment] = useState<Fragment | null>(null);
 
   return (
     <div className="h-screen">
@@ -25,16 +25,21 @@ export const ProjectView = ({ projectId }: Props) => {
           minSize={20}
           className="flex flex-col min-h-0"
         >
+          <Suspense fallback={<p>Loading project...</p>}>
+            <ProjectHeader projectId={projectId} />
+          </Suspense>
           <Suspense fallback={<p>Loading messages...</p>}>
-          <MessagesContainer projectId={projectId} />
+            <MessagesContainer
+              projectId={projectId}
+              activeFragment={activeFragment}
+              setActiveFragment={setActiveFragment}
+            />
           </Suspense>
         </ResizablePanel>
         <ResizableHandle withHandle />
-        <ResizablePanel
-          defaultSize={65}
-          minSize={50}
-        > TODO : Preview
-          
+        <ResizablePanel defaultSize={65} minSize={50}>
+          {" "}
+          TODO : Preview
         </ResizablePanel>
       </ResizablePanelGroup>
     </div>
